f24fe923cabc9e59934c38efb0591b1d3b59862c
wangqr <wangqr@wangqr.tk>
2019-11-02 02:26:36 -0400

    Fix bugs in ram audio provider
    There was some magic bit operations to calculate the cache block offsets. This only works when both bytes_per_sample and channels are power of 2. Originally the format is assumed to be int16 mono, which satisfies this requirement. However in case we use original audio data, the channels can be something not a power of 2 (e.g. for 5.1 channel audio the number of channels is 6). This will break the calculation. We rewrite the calculation, without using those bit operations.

--- libaegisub/audio/provider_ram.cpp
+++ libaegisub/audio/provider_ram.cpp
@@ -77,9 +77,11 @@ void RAMAudioProvider::FillBuffer(void *buf, int64_t start, int64_t count) const
 			break;
 		}
 
-		const size_t i = (start * bytes_per_sample * channels) >> CacheBits;
-		const int start_offset = (start * bytes_per_sample * channels) & (CacheBlockSize-1);
-		const int read_size = std::min<int>(bytes_remaining, CacheBlockSize - start_offset);
+		const int64_t samples_per_block = CacheBlockSize / bytes_per_sample / channels;
+
+		const size_t i = start / samples_per_block;
+		const int start_offset = (start % samples_per_block) * bytes_per_sample * channels;
+		const int read_size = std::min<int>(bytes_remaining, samples_per_block * bytes_per_sample * channels - start_offset);
 
 		memcpy(charbuf, &blockcache[i][start_offset], read_size);
 		charbuf += read_size;
