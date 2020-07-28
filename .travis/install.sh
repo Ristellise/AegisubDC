3bde4dd751f1a1b717caeb878e83aa0b208d3ad0
wangqr <wangqr@wangqr.tk>
2020-07-28 16:43:53 -0400

    Move coveralls to after_success in Travis CI
    So it won't affect the build result

--- .travis/install.sh
+++ .travis/install.sh
@@ -6,14 +6,16 @@ if [ "$TRAVIS_OS_NAME" = 'linux' ]; then
   # Remove the CMake provided by travis
   sudo rm -rf /usr/local/cmake*
   if [ "$BUILD_SUIT" = 'autotools' ]; then
-    sudo pip install -U cpp-coveralls;
-    git submodule --quiet init;
-    git submodule --quiet update vendor/googletest;
+    if [ ! -z "$WITH_COVERALLS" ]; then
+      sudo pip3 install -U cpp-coveralls
+    fi
+    git submodule --quiet init
+    git submodule --quiet update vendor/googletest
   else
-    pushd /usr/src/googletest;
-    sudo cmake .;
-    sudo make install -j2;
-    popd;
+    pushd /usr/src/googletest
+    sudo cmake .
+    sudo make install -j2
+    popd
   fi
 fi
 sudo luarocks install busted > /dev/null
