533716c6c0f2ba6f5f661fa299d47d3a90895204
wangqr <wangqr@wangqr.tk>
2020-07-28 16:32:26 -0400

    Add default path on Windows in FindAviSynth.cmake

--- cmake/FindAviSynth.cmake
+++ cmake/FindAviSynth.cmake
@@ -2,14 +2,24 @@ find_package(PkgConfig QUIET)
 pkg_check_modules(PC_AviSynth QUIET avisynth)
 find_path(AviSynth_INCLUDE_DIRS
   NAMES avisynth.h
+  PATHS "C:/Program Files/AviSynth+/FilterSDK/include" "C:/Program Files (x86)/AviSynth+/FilterSDK/include"
   PATH_SUFFIXES avisynth
   HINTS ${PC_AviSynth_INCLUDE_DIRS}
 )
-find_library(AviSynth_SHARED_LIBRARY
-  NAMES avisynth
-  PATH_SUFFIXES c_api
-  HINTS ${PC_AviSynth_LIBRARY_DIRS}
-)
+if(WIN32)
+  find_file(AviSynth_SHARED_LIBRARY
+    NAMES AviSynth.dll
+    PATHS "C:/Windows/System32/"
+    PATH_SUFFIXES c_api
+    HINTS ${PC_AviSynth_LIBRARY_DIRS}
+  )
+else()
+  find_library(AviSynth_SHARED_LIBRARY
+    NAMES avisynth
+    PATH_SUFFIXES c_api
+    HINTS ${PC_AviSynth_LIBRARY_DIRS}
+  )
+endif()
 set(AviSynth_VERSION ${PC_AviSynth_VERSION})
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(AviSynth
